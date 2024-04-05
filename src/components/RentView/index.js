32264eb9d739f9b22ba16eb59db5077e6d666ca9
LpxsBr <anselmolopes.an@gmail.com>
2024-04-04 23:11:48 -0300

    fix: info and logic of calc in the component

--- src/components/RentView/index.js
+++ src/components/RentView/index.js
@@ -43,6 +43,49 @@ export default function RentView({ func, data }) {
         }
     }
 
+    const calcBetweenTwoDates = (start = '', end = '') => {
+
+        const format1 = (value = '') => {
+            var dt = value.split('/')
+            return `${dt[2]}-${dt[0]}-${dt[1]}`
+        }
+
+        const format2 = (value = '') => {
+            var dt = value.split('/')
+            return `${dt[2]}-${dt[1]}-${dt[0]}`
+        }
+
+
+        if (new Date(format1(end)) == new Date(format1(start)) && !(new Date(format1(end)) != 'Invalid Date' || new Date(format1(start)) != 'Invalid Date')) {
+            return 1
+        }
+
+        if (new Date(format1(end)) != 'Invalid Date' && new Date(format1(start)) != 'Invalid Date') {
+            console.log('UMM');
+            const diffTime = new Date(format1(end)) - new Date(format1(start));
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+            return Number(diffDays) + 1;
+        }
+
+
+        console.log('DOIS');
+        const diffTime = new Date(format2(end)) - new Date(format2(start));
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        return Number(diffDays) + 1;
+
+    }
+
+    const formatToCalcDate = (value = '') => {
+        var dt = value.split('/')
+        return `${dt[2]}-${dt[0]}-${dt[1]}`
+    }
+
+    console.log(data)
+
+    console.log(data?.start_date)
+    console.log(formatToCalcDate(data?.start_date))
+    console.log(new Date(formatToCalcDate(data?.start_date)))
+
     return (
         <div className={"modal"} style={{ display: 'block', boxShadow: '1px 2px 13px -5px #396fae', backgroundColor: 'rgba(57, 111, 174, 0.2)' }}>
             <div className="modal-dialog" style={{ maxWidth: 'none', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
@@ -53,7 +96,7 @@ export default function RentView({ func, data }) {
                     </div>
                     <div className="modal-body" style={{ height: 'calc(100vh - 150px)' }}>
                         <div className="row">
-                            <div className="card">
+                            <div className="card" style={{ padding: 8}}>
                                 <div className="row">
                                     <h6>Informações do cliente</h6>
                                 </div>
@@ -66,8 +109,8 @@ export default function RentView({ func, data }) {
                                         <p>{data?.client?.name} {data?.client?.surname}</p>
                                     </div>
                                     <div className="col" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', justifyContent: 'flex-start' }}>
-                                        <div style={{ backgroundColor: data?.client?.completed ? 'green' : 'blue', color: '#fff', height: '30px', borderRadius: '8px', width: '130px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                                            {data?.completed ? 'Concluído' : 'Em andamento'}
+                                        <div style={{ backgroundColor: data?.client?.completed ? 'green' : (new Date() > new Date(data?.end_date) ? 'red' : 'blue'), color: '#fff', height: '30px', borderRadius: '8px', width: '130px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                            {data?.completed ? 'Concluído' : (new Date() > new Date(data?.end_date) ? 'Atrasado' : 'Em andamento')}
                                         </div>
                                     </div>
                                     <div className="col">
@@ -94,13 +137,13 @@ export default function RentView({ func, data }) {
                                         <strong>
                                             <label htmlFor="modalName" className="form-label">Início</label>
                                         </strong>
-                                        <IMaskInput type={'date'} className="form-control" value={data?.start_date} disabled/>
+                                        <IMaskInput type={'date'} className="form-control" value={data?.start_date} disabled />
                                     </div>
                                     <div className="col-3">
                                         <strong>
                                             <label htmlFor="modalName" className="form-label">Fim</label>
                                         </strong>
-                                        <IMaskInput type={'date'} className="form-control" value={data?.end_date} disabled/>
+                                        <IMaskInput type={'date'} className="form-control" value={data?.end_date} disabled />
                                     </div>
                                 </div>
                             </div>
@@ -108,19 +151,21 @@ export default function RentView({ func, data }) {
                         <div className="row">
                             <DataTable value={data?.products || []} scrollable scrollHeight="200px" selectionMode="single" paginator rows={5} rowsPerPageOptions={[5, 10]} tableStyle={{ minWidth: '300px' }}
                                 paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
-                                currentPageReportTemplate="{first} to {last} of {totalRecords}"
+                                currentPageReportTemplate="Item {first} ao {last} de {totalRecords}"
                                 sortField="id" sortOrder={1}>
-                                <Column field="id" header="ID" style={{ width: '20%' }}></Column>
-                                <Column field="product.description" header="Produto" style={{ width: '20%' }}></Column>
-                                <Column field="product_quantity" header="Qtd" style={{ width: '20%' }}></Column>
-                                <Column field="daily_price" header="Valor" body={priceFormat} style={{ width: '20%' }}></Column>
+                                <Column field="id"                  header="ID"                             style={{ width: '20%' }}></Column>
+                                <Column field="product.description" header="Produto"                        style={{ width: '20%' }}></Column>
+                                <Column field="product_quantity"    header="Qtd"                            style={{ width: '20%' }}></Column>
+                                {/* <Column field="daily_value"         header="Diária"     body={priceFormat}  style={{ width: '20%' }}></Column> */}
+                                <Column field="daily_price"         header="Total"      body={priceFormat}  style={{ width: '20%' }}></Column>
                             </DataTable>
                         </div>
                     </div>
                     <div className="modal-footer" style={{ display: "flex", alignItems: "center", justifyContent: "space-evenly" }}>
-                        <button type="button" className="btn btn-danger" onClick={()=>handleDeleteRent()}>Cancelar</button>
-                        <h5>{priceFormatTotal(Number(data?.total_daily_price))}</h5>
-                        <button type="button" className="btn btn-success" onClick={()=>handleReceiveRent()} >Receber</button>
+                        <button type="button" className="btn btn-danger" onClick={() => handleDeleteRent()}>Cancelar</button>
+                        {/* <h1>{calcBetweenTwoDates(data?.start_date, data?.end_date)}</h1> */}
+                        <h5>{priceFormatTotal(Number(data?.total_daily_price) * calcBetweenTwoDates(data?.start_date, data?.end_date))}</h5>
+                        <button type="button" className="btn btn-success" onClick={() => handleReceiveRent()} >Receber</button>
                     </div>
                 </div>
             </div>
